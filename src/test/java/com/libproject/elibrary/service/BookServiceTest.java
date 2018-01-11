30f754912779fdfeca3e848fa8bb3202942117a3
Alexander Voloshyn <voloshyn.olexander@gmail.com>
2018-01-11 08:54:59 +0200

    Mockito dependency

---
+++ src/test/java/com/libproject/elibrary/service/BookServiceTest.java
@@ -0,0 +1,12 @@
+package com.libproject.elibrary.service;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BookServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
