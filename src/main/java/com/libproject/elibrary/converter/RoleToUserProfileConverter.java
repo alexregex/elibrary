4668c8755cc66d9f2a33f45aee5f1b33d7f09c90
Alexander Voloshyn <voloshyn.olexander@gmail.com>
2018-01-14 09:21:25 +0200

    optimize imports

--- src/main/java/com/libproject/elibrary/converter/RoleToUserProfileConverter.java
+++ src/main/java/com/libproject/elibrary/converter/RoleToUserProfileConverter.java
@@ -2,8 +2,6 @@ package com.libproject.elibrary.converter;
 
 import com.libproject.elibrary.model.UserProfile;
 import com.libproject.elibrary.service.UserProfileService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
