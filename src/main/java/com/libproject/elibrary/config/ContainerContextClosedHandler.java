25a7c33c32374eff7fcd91f5a1fe72644ca36436
Alexander Voloshyn <voloshyn.olexander@gmail.com>
2018-01-17 22:42:40 +0200

    Resolve WARNING: org.apache.catalina.loader.WebappClassLoaderBase.clearReferencesJdbc The web application [ROOT] registered the JDBC driver

---
+++ src/main/java/com/libproject/elibrary/config/ContainerContextClosedHandler.java
@@ -0,0 +1,46 @@
+package com.libproject.elibrary.config;
+
+import com.mysql.jdbc.AbandonedConnectionCleanupThread;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+/*Resolve WARNING: org.apache.catalina.loader.WebappClassLoaderBase.clearReferencesJdbc The web application [ROOT] registered the JDBC driver*/
+@WebListener
+public class ContainerContextClosedHandler implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+        java.sql.Driver driver;
+
+        // clear drivers
+        while(drivers.hasMoreElements()) {
+            try {
+                driver = drivers.nextElement();
+                DriverManager.deregisterDriver(driver);
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        // MySQL driver leaves around a thread. This static method cleans it up.
+        try {
+            AbandonedConnectionCleanupThread.shutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
